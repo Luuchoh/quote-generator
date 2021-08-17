0a673fec1d4b74dd3bb8bd7997a9eb554b21629b
Luuchoh <luxo997@gmail.com>
2021-08-16 21:37:41 -0500

    quitando sdk twitter

--- src/components/QuotesComp.jsx
+++ src/components/QuotesComp.jsx
@@ -5,25 +5,6 @@ import Twitter from "twitter";
 import "../styles/button-styles.css";
 
 const QuotesComp = () => {
-  const client = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-  });
-
-  const NewTweet = () => {
-    client
-      .post("statuses/update", {
-        status: "I Love Twitter",
-      })
-      .then(function (tweet) {
-        console.log(tweet);
-      })
-      .catch(function (error) {
-        throw error;
-      });
-  };
 
   const [quote, setQuote] = useState("");
 
